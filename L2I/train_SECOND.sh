1f389a66259b72b64c2f30240129b62a7d95c20f
MUYI959 <1539242261@qq.com>
2024-12-16 21:23:50 +0800

    upload json

--- L2I/train_SECOND.sh
+++ L2I/train_SECOND.sh
@@ -5,7 +5,7 @@ mkdir logs
 #    -n exp_SECOND_from_SCD \
 #    --gpus 0, \
 #    --data_root /home/L2I/data/SECOND \
-#    --json_file /home/L2I/data/SECOND_train_concat_psd12_from_SCD_0.8.json >> logs/SECOND_allpsd12_512_from_SCD.file 2>&1 &
+#    --json_file /home/L2I/data/SECOND_train_0.8.json >> logs/SECOND_allpsd12_512_from_SCD.file 2>&1 &
 
 CUDA_VISIBLE_DEVICES=7 nohup python main.py --base configs/stable-diffusion/v1-finetune_SECOND.yaml \
     -t \
@@ -14,7 +14,7 @@ CUDA_VISIBLE_DEVICES=7 nohup python main.py --base configs/stable-diffusion/v1-f
     --gpus 0, \
     --data_root /home/L2I/data/SECOND \
     --no-test \
-    --json_file /home/L2I/data/SECOND_train_concat_psd12_from_SCD_0.8.json > logs/SECOND_allpsd12_512_from_SCD.file 2>&1 &
+    --json_file /home/L2I/data/SECOND_train_0.8.json > logs/SECOND_allpsd12_512_from_SCD.file 2>&1 &
 
 
 CUDA_VISIBLE_DEVICES=0 python main.py --base configs/stable-diffusion/v1-finetune_SECOND.yaml \
@@ -24,4 +24,4 @@ CUDA_VISIBLE_DEVICES=0 python main.py --base configs/stable-diffusion/v1-finetun
     --gpus 0, \
     --data_root /home/L2I/data/SECOND \
     --no-test \
-    --json_file /data/yrz/repos/FreeStyleNet/data/SECOND_train_concat_psd12_from_SCD_0.8.json
+    --json_file home/L2I/data/SECOND_train_0.8.json
